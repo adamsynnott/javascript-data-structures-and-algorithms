ea757d36e8f2bd266b2be73211bb9c59e8233753
Adam Synnott <adam.synnott@theiconic.com.au>
2020-09-17 21:06:09 +1000

    Added jest dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   env: {
     node: true,
+    'jest/globals': true,
   },
   extends: 'airbnb',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: ['jest'],
 };
